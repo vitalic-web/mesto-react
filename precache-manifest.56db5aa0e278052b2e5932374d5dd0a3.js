f21e6d9330bbc38176e212ec4ec0d7868be9b995
Vitalic-web <vitalic.web@gmail.com>
2020-09-04 19:23:14 +0200

    Updates

--- precache-manifest.56db5aa0e278052b2e5932374d5dd0a3.js
+++ precache-manifest.56db5aa0e278052b2e5932374d5dd0a3.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "692e6a5753394fe2534e0a79967117ca",
+    "revision": "6185c7ef5d1be46acb5964c1df8c4a90",
     "url": "/index.html"
   },
   {
-    "revision": "334654e5294641f8ca91",
+    "revision": "f35a5c2a6de6e95584c3",
     "url": "/static/css/main.0aaf2af3.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.bfa99d5d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "334654e5294641f8ca91",
-    "url": "/static/js/main.19d13052.chunk.js"
+    "revision": "f35a5c2a6de6e95584c3",
+    "url": "/static/js/main.d5ca8db7.chunk.js"
   },
   {
     "revision": "c5e7b408ed1922744b94",
